2c2136807822054149e937848de025fa0a264f60
Jordan <jordan.henkelman@gmail.com>
2023-02-14 00:04:02 -0700

    Documentation completed

--- Assignment-1/YourCode/your-code.py
+++ Assignment-1/YourCode/your-code.py
@@ -1,16 +1,15 @@
-# Skeleton for commit and roll-back exercise
 # Darylle, Jon, Jordan, Shargeel
-# *** Your Code goes Here ***
 from tabulate import tabulate
 import csv
 
+# variables
 dataPath = "Assignment-1/Data-Assignment-1/csv/"
 mainMemory = []
 log_transactions = []
 log = None
 transaction_success = ""
 
-# column headers
+# table column headers
 cust_columns = ["ID", "LastName", "FirstName",
                 "Address", "City", "Age"]
 account_columns = ["ID", "Checking Account", "Savings Account"]
@@ -18,13 +17,16 @@ accbal_columns = ["Account", "Balance"]
 log_columns = ["Transaction ID", "Amount", "Completed",
                "User ID", "Account Number", "Account Type", "Balance Before", "Current Balance"]
 
+# reads .csv files and turns them into/returns an array
+
 
 def myreader(filename: str):
     with open(filename, newline='') as f:
         reader = csv.reader(f)
         yourList = list(reader)
-
     return yourList
+
+# writes an array to a .csv file
 
 
 def mywriter(filename: str, mylist: list):
@@ -33,12 +35,16 @@ def mywriter(filename: str, mylist: list):
         # write multiple rows
         writer.writerows(mylist)
 
+# prints the log to a table
+
 
 def print_log(logList):
     print("Log Transactions")
     print("----------------")
     print(tabulate(logList, headers=log_columns))
     print("\n")
+
+# prints tables
 
 
 def print_tables():
@@ -49,6 +55,8 @@ def print_tables():
     print("\n")
     print(tabulate(mainMemory[2], headers=accbal_columns))
     print("\n")
+
+# log object
 
 
 class Log:
@@ -63,16 +71,12 @@ class Log:
         self.funds_before = funds_before
         self.funds_after = funds_after
 
+    # returns values of log
     def __dir__(self):
         return [self.trans_id, self.amount, self.completed, self.userID, self.acc_num, self.acct_type, self.funds_before, self.funds_after]
 
-# Your main program
-
 
 def main():
-    # print("First Output)
-    # print("Print Original Contents of Databases")
-    # print("Print current status of Log Sub-system\n\n")
     global transaction_success
 
     # loading lists
@@ -83,12 +87,8 @@ def main():
     balList = myreader(dataPath + 'account-balance.csv')
     mainMemory.append(balList)
 
-    # all these variables are hard coded. Can make loops and find them iteratively if we need to.
-    emma_id = mainMemory[0][2][0]
     emma_sav_acc = mainMemory[1][2][2]
     emma_cheq_acc = mainMemory[1][2][1]
-    emma_init_sav_bal = mainMemory[2][9][1]
-    emma_init_cheq_bal = mainMemory[2][4][1]
 
     # printing main memory before transactions
     print("\nBEFORE Transaction Block 1")
@@ -108,6 +108,7 @@ def main():
             cheq_before_bal = int(m[1])
             int_cheq_bal -= transaction_amt
             m[1] = str(int_cheq_bal)
+            # if balance before == balance after, transaction is unsuccessful and prints error message
             if (cheq_before_bal == int_cheq_bal):
                 log = Log(logID, "Chequing", -transaction_amt,
                           False, '3', emma_cheq_acc, cheq_before_bal, int_cheq_bal)
@@ -126,6 +127,7 @@ def main():
             sav_before_bal = int(m[1])
             int_sav_bal += transaction_amt
             m[1] = str(int_sav_bal)
+            # if balance before == balance after, transaction is unsuccessful and prints error message
             if (sav_before_bal == int_sav_bal):
                 log = Log(logID, "Savings", transaction_amt,
                           False, '3', emma_sav_acc, sav_before_bal, int_sav_bal)
@@ -150,12 +152,14 @@ def main():
     mywriter(dataPath + 'account-balance.csv', mainMemory[2])
 
     # BLOCK 2 -------------------------------------------------------
+    # taking the money out of chequing
     for m in mainMemory[2]:
         if (m[0] == emma_cheq_acc):
             int_cheq_bal = int(m[1])
             cheq_before_bal = int(m[1])
             int_cheq_bal -= transaction_amt
             m[1] = str(int_cheq_bal)
+            # if balance before == balance after, transaction is unsuccessful and prints error message
             if (sav_before_bal == int_sav_bal):
                 log = Log(logID, "Chequing", -transaction_amt,
                           False, '3', emma_cheq_acc, cheq_before_bal, int_cheq_bal)
@@ -167,12 +171,14 @@ def main():
             log_transactions.append(log.__dir__())
             logID += 1
 
+    # depositing the money into savings (failure)
     for m in mainMemory[2]:
         if (m[0] == emma_sav_acc):
             int_sav_bal = int(m[1])
             sav_before_bal = int(m[1])
             #int_sav_bal += transaction_amt
             m[1] = str(int_sav_bal)
+            # if balance before == balance after, transaction is unsuccessful and prints error message
             if (sav_before_bal == int_sav_bal):
                 log = Log(logID, "Savings", transaction_amt,
                           False, '3', emma_sav_acc, sav_before_bal, int_sav_bal)
@@ -199,22 +205,10 @@ def main():
     # externally save log
     mywriter(dataPath + 'log.csv', log_transactions)
 
-    # Transaction Block 1: Successful
-    # print("BLOCK TRANSACTION 1")
-    # print("Subtract money from one account.")
-    # print("Add money to second one")
-    # print("COMMIT all your changes")
-    # print("Print Contents of Databases")
-    # print("Print current status of Log Sub-system\n\n")
-
-    # Transaction Block 1: Fails!
-    # print("BLOCK TRANSACTION 2")
-    # print("Subtract money from one account (Same Transaction than before)")
-    # print("Failure occurs!!!!!!! ACTION REQUIRED")
-    # print("Must either AUTOMATICALLY Roll-back Database to a state of equilibrium (Bonus), OR\nSTOP Operations and show: (a) Log-Status, and (b) Databases Contents.\n")
-    # print("\nThe Log Sub-system contents should show the necessary operations needed to fix the situation!")
-
-    # transaction block 2
+    # COMMIT changes
+    mywriter(dataPath + 'customer.csv', mainMemory[0])
+    mywriter(dataPath + 'account.csv', mainMemory[1])
+    mywriter(dataPath + 'account-balance.csv', mainMemory[2])
 
 
 main()
